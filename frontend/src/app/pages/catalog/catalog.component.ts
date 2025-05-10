ff8c994dabecd793de918d05ec542e57caeb483d
Diegod <diegodelgado685@gmail.com>
2025-05-09 23:08:21 -0500

    Refactor CatalogComponent to use CommonModule and initialize catalogItems from CatalogService

--- frontend/src/app/pages/catalog/catalog.component.ts
+++ frontend/src/app/pages/catalog/catalog.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CatalogService } from '../../core/services/catalog/catalog.service';
+import { CatalogItem } from '../../core/models/catalog.model';
 
 @Component({
   selector: 'app-catalog',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
+  catalogItems: CatalogItem[] = [];
 
+  constructor(private catalogService: CatalogService) {
+    this.catalogService.getCatalog().subscribe((data) => {
+      this.catalogItems = data;
+    });
+  }
 }
