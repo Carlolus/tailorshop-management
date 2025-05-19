7fa7238004af22863a19fa64cfe257c8c0e815ac
Carlos <carlos.cg170404@gmail.com>
2025-05-19 12:57:24 -0500

    Updated fabric service, adding create, update and delete

--- frontend/src/app/core/services/fabrics/fabric.service.ts
+++ frontend/src/app/core/services/fabrics/fabric.service.ts
@@ -2,16 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Fabric } from '../../models/fabric.model';
+import { HttpHeaderService } from '../httpHeader';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FabricService {
-  private apiUrl = 'http://localhost:3000/';
-  constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:3000/fabrics';
+  constructor(
+    private http: HttpClient,
+    private httpHeaderService: HttpHeaderService
+  ){ }
 
   getFabrics(): Observable<Fabric[]> {
-    const url = `${this.apiUrl}fabrics`
-    return this.http.get<Fabric[]>(url);
+    return this.http.get<Fabric[]>(this.apiUrl);
+  }
+
+  createFabric(fabric: Omit<Fabric, 'fabric_id'>): Observable<Fabric> {
+    return this.http.post<Fabric>(this.apiUrl, fabric);
+  }
+
+  updateFabric(fabric: Fabric): Observable<Fabric> {
+    return this.http.put<Fabric>(`${this.apiUrl}/${fabric.fabric_id}`, fabric);
+  }
+
+  deleteFabric(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
