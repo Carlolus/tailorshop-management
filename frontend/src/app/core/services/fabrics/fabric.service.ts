6965234dfd90a8329dc4b93bdba76f9756bf0b7b
Carlos <carlos.cg170404@gmail.com>
2025-05-19 13:17:00 -0500

    Updated fabric service, now headers working

--- frontend/src/app/core/services/fabrics/fabric.service.ts
+++ frontend/src/app/core/services/fabrics/fabric.service.ts
@@ -9,24 +9,32 @@ import { HttpHeaderService } from '../httpHeader';
 })
 export class FabricService {
   private apiUrl = 'http://localhost:3000/fabrics';
+
   constructor(
     private http: HttpClient,
     private httpHeaderService: HttpHeaderService
-  ){ }
+  ) {}
 
   getFabrics(): Observable<Fabric[]> {
-    return this.http.get<Fabric[]>(this.apiUrl);
+    return this.http.get<Fabric[]>(this.apiUrl, { headers: this.httpHeaderService.getHeaders() });
   }
 
   createFabric(fabric: Omit<Fabric, 'fabric_id'>): Observable<Fabric> {
-    return this.http.post<Fabric>(this.apiUrl, fabric);
+    return this.http.post<Fabric>(this.apiUrl, fabric, { headers: this.httpHeaderService.getHeaders() });
   }
 
   updateFabric(fabric: Fabric): Observable<Fabric> {
-    return this.http.put<Fabric>(`${this.apiUrl}/${fabric.fabric_id}`, fabric);
+    return this.http.put<Fabric>(
+      `${this.apiUrl}/${fabric.fabric_id}`,
+      fabric,
+      { headers: this.httpHeaderService.getHeaders() }
+    );
   }
 
   deleteFabric(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(
+      `${this.apiUrl}/${id}`,
+      { headers: this.httpHeaderService.getHeaders() }
+    );
   }
 }
