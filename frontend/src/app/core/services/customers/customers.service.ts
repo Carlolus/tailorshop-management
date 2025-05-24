a31ee1b7363913af4afb83ec116c58b764c8f7ea
Carlos <carlos.cg170404@gmail.com>
2025-05-23 20:58:24 -0500

    Add CustomerService to handle customer-related API operations

---
+++ frontend/src/app/core/services/customers/customers.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from '../../models/customer.model';
+import { HttpHeaderService } from '../httpHeader';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private apiUrl = 'http://localhost:3000/customers';
+
+  constructor(
+    private http: HttpClient,
+    private httpHeaderService: HttpHeaderService
+  ) {}
+
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl, { headers: this.httpHeaderService.getHeaders() });
+  }
+
+  createCustomer(customer: Omit<Customer, 'customer_id'>): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer, { headers: this.httpHeaderService.getHeaders() });
+  }
+
+  updateCustomer(customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(
+      `${this.apiUrl}/${customer.customer_id}`,
+      customer,
+      { headers: this.httpHeaderService.getHeaders() }
+    );
+  }
+
+  deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiUrl}/${id}`,
+      { headers: this.httpHeaderService.getHeaders() }
+    );
+  }
+}
